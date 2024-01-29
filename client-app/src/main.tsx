5ca3dd3a67ea50014fdb4316e0099b9c497ddf9a
Alex Alfred <alex.alfred494@gmail.com>
2024-01-28 21:18:54 -0600

    add Mobx state mgmt

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -2,7 +2,10 @@ import ReactDOM from 'react-dom/client'
 import App from './app/layout/App.tsx'
 import './app/layout/styles.css'
 import 'semantic-ui-css/semantic.min.css'
+import { StoreContext, store } from './app/stores/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <StoreContext.Provider value={store}>
+        <App />
+    </StoreContext.Provider>
 )
